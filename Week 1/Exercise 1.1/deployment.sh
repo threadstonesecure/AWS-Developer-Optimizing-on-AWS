e284b0b7e65675cccf875cdcf302b49709bc2e5b
EC2 Default User <ec2-user@ip-172-31-45-64.ec2.internal>
2019-03-12 07:13:11 +0000

    Draft of Exercise 1.2

--- Week 1/Exercise 1.1/deployment.sh
+++ Week 1/Exercise 1.1/deployment.sh
@@ -2,16 +2,14 @@ export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/plac
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 SourceBucket=sourcebucketname$AWSAccountId
 
-aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
-
-aws cloudformation create-stack --stack-name edx-project-stack --template-body file://iam.yaml \
+aws cloudformation create-stack --stack-name edx-project-iam-stack --template-body file://iam.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters ParameterKey=Password,ParameterValue=P@ssw0rd 
  
-aws cloudformation wait stack-create-complete --stack-name edx-project-stack
-AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+aws cloudformation wait stack-create-complete --stack-name edx-project-iam-stack
+AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-iam-stack \
 --query 'Stacks[0].Outputs[?OutputKey==`AccessKey`].OutputValue' --output text)
-AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-projec-iamt-stack \
 --query 'Stacks[0].Outputs[?OutputKey==`SecretKey`].OutputValue' --output text)
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
--- Week 1/Exercise 1.2/deployment.sh
+++ Week 1/Exercise 1.2/deployment.sh
@@ -2,9 +2,22 @@ export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/plac
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 SourceBucket=sourcebucketname$AWSAccountId
 
-aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
-
-aws cloudformation create-stack --stack-name edx-project-ecr-stack --template-body file://ecr.yaml
+# aws cloudformation create-stack --stack-name edx-project-ecr-stack --template-body file://ecr.yaml
  
-aws cloudformation wait stack-create-complete --stack-name edx-project-ecr-stack
-echo -e "${RED}Exercise 1.2 ECR Stack created!${NOCOLOR}"   
+# aws cloudformation wait stack-create-complete --stack-name edx-project-ecr-stack
+# echo -e "${RED}Exercise 1.2 ECR Stack created!${NOCOLOR}"   
+
+# $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+# docker tag timezones-appserver:latest $AWSAccountId.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/timezones-appserver:latest
+# docker push $AWSAccountId.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/timezones-appserver:latest
+# docker tag timezones-frontend:latest $AWSAccountId.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/timezones-frontend:latest
+# docker push $AWSAccountId.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/timezones-frontend:latest
+# docker images
+
+aws iam create-service-linked-role --aws-service-name ecs.amazonaws.com
+aws cloudformation create-stack --stack-name edx-project-ecs-stack --template-body file://ecs.yaml \
+--capabilities CAPABILITY_NAMED_IAM
+ 
+aws cloudformation wait stack-create-complete --stack-name edx-project-ecs-stack
+echo -e "${RED}Exercise 1.2 ECS Stack created!${NOCOLOR}"  
+
