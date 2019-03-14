2ae1d74ea41a34a6800657ea560ecf68d734df43
EC2 Default User <ec2-user@ip-172-31-43-69.ec2.internal>
2019-03-14 09:19:14 +0000

    Draft of Exercise 5.1

--- Week 4/Exercise 4.1/lab.sh
+++ Week 4/Exercise 4.1/lab.sh
@@ -2,6 +2,7 @@ wget -c https://us-west-2-tcdev.s3.amazonaws.com/courses/AWS-100-ADO/v1.0.0/exer
 cd ~/environment
 unzip -o ~/ex-dynamodbscale
 cd ex-dynamodbscale
+sed -i "s/    print(\"PutItem succeeded:\")/print(\"1000 PutItem succeeded:\")/g" put_item_load_test.py
 python3 put_item_load_test.py
 echo "Sleep 10s"
 sleep 10s
@@ -9,7 +10,7 @@ python3 put_item_load_test.py
 echo "Sleep 10s"
 sleep 10s
 python3 put_item_load_test.py
-echo "Sleep1m"
+echo "Sleep 1m"
 sleep 1m
 python3 put_item_load_test.py
 echo "Sleep 10s"
---
+++ Week 5/Exercise 5.1/deployment.sh
@@ -0,0 +1,22 @@
+export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
+SourceBucket=sourcebucketname$AWSAccountId
+
+aws cloudformation update-stack --stack-name edx-project-iam-stack --template-body file://iam.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters ParameterKey=Password,ParameterValue=P@ssw0rd 
+ 
+aws cloudformation wait stack-update-complete --stack-name edx-project-iam-stack
+AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-iam-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`AccessKey`].OutputValue' --output text)
+AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-project-iam-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`SecretKey`].OutputValue' --output text)
+DenyBucketDeletionPolicy=$(aws cloudformation describe-stacks --stack-name edx-project-iam-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`DenyBucketDeletionPolicy`].OutputValue' --output text)
+AllowBucketDeletionPolicy=$(aws cloudformation describe-stacks --stack-name edx-project-iam-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`AllowBucketDeletionPolicy`].OutputValue' --output text)
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
+echo -e "${RED}Exercise 5.1 IAM Stack updated!${NOCOLOR}"   
