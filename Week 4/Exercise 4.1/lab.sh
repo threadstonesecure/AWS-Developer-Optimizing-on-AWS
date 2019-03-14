42973a7ea8658e42f6bb9a39d4a3d3dffc139532
EC2 Default User <ec2-user@ip-172-31-38-36.ec2.internal>
2019-03-14 07:35:04 +0000

    Draft of Exercise 4.2

--- Week 4/Exercise 4.1/lab.sh
+++ Week 4/Exercise 4.1/lab.sh
@@ -9,6 +9,15 @@ python3 put_item_load_test.py
 echo "Sleep 10s"
 sleep 10s
 python3 put_item_load_test.py
-# sleep 10m
-# cd ~/environment/ex-dynamodb
-# python3 put_ddb_item.py
+echo "Sleep1m"
+sleep 1m
+python3 put_item_load_test.py
+echo "Sleep 10s"
+sleep 10s
+python3 put_item_load_test.py
+echo "Sleep 10s"
+sleep 10s
+python3 put_item_load_test.py
+sleep 10m
+cd ~/environment/ex-dynamodb
+python3 put_ddb_item.py
